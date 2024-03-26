966cfdf7a6fc3009f16a41517f6cd4b1071a71a0
szagyarorszag <bakitzhana566@gmail.com>
2024-03-26 21:58:36 +0500

    7started

--- Lab4/product-app/src/app/app.module.ts
+++ Lab4/product-app/src/app/app.module.ts
@@ -7,10 +7,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
-import { ShippingComponent } from './shipping/shipping.component';
+import { ShippingComponent } from "./shipping/shipping.component";
 
 @NgModule({
     imports: [
--- Lab4/product-app/src/app/cart/cart.component.ts
+++ Lab4/product-app/src/app/cart/cart.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-
 import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
